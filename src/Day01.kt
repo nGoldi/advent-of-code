5f8af6270a23ed29af85c329ac927315fa10640a
nicolas <nicolas.goldammer@sevdesk.de>
2022-12-01 23:08:45 +0100

    solution day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,19 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>, count: Int): Int {
+        return input.fold(mutableListOf(0)) { acc, s ->
+            when {
+                s.isNotBlank() -> acc[acc.lastIndex] = s.toInt() + acc.last()
+                else -> acc.add(0)
+            }
+            acc
+        }
+            .sortedDescending()
+            .take(count)
+            .sum()
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println(part1(input, 1))
+    println(part1(input, 3))
 }
