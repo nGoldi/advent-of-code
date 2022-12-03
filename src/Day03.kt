51c473dd8933354e75bab00f66cb76f2f26945ce
nicolas <nicolas.goldammer@sevdesk.de>
2022-12-03 20:35:51 +0100

    add day03

---
+++ src/Day03.kt
@@ -0,0 +1,29 @@
+fun main() {
+    val input = readInput("Day03")
+    println(part1(input))
+    println(part2(input))
+}
+
+
+
+private fun calculatePriority(char: Char): Int {
+    return if (char.code >= 92) {
+        char.code - 96
+    } else {
+        char.code - 38
+    }
+}
+
+private fun calculatePriority(backpacks: List<String>): Int {
+    return calculatePriority(backpacks.map { it.toCharArray().toSet() }.reduce { acc, backpack -> backpack.intersect(acc) }.first())
+}
+
+fun part1(backpacks: List<String>): Int {
+    return backpacks.sumOf { calculatePriority(it.chunked(it.length / 2)) }
+}
+
+fun part2(backpacks: List<String>): Int {
+    return backpacks.windowed(3, step = 3).sumOf { calculatePriority(it) }
+}
+
+
